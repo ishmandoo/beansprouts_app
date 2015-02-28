f48d4e6baddd5ea38521368c97eb0f30d1879370
Ben <ishmandoo@gmail.com>
2015-02-28 10:41:06 -0500

    initial commit of loopback

---
+++ server/boot/create-sample-models.js
@@ -0,0 +1,15 @@
+module.exports = function(app) {
+  /*app.dataSources.beansprouts_db.automigrate('Student', function(err) {
+    if (err) throw err;
+
+    app.models.students.create([
+      {name: 'Bilbo'},
+      {name: 'Ambo'},
+    ], function(err, coffeeShops) {
+      if (err) throw err;
+
+      console.log('Models created: \n', coffeeShops);
+    });
+  });
+  */
+};
